26e6e5700a18992e86d2e03978b45cd7803180c7
Ivailo Aleksandrov <ivailoaleksandrov123@gmail.com>
2022-08-06 10:32:13 +0300

    More Exercises

---
+++ Programming Basics CSharp/More exercises/01/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace _01_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int student = 0;  // брой билети студент
+            int standart = 0;
+            int kid = 0;
+            string input = Console.ReadLine();     // име на филма до команди "Finish"
+
+            while (input != "Finish")
+            {
+                int freeSeats = int.Parse(Console.ReadLine());  // брой свободни седалки от 1 до 100 
+                string typeTicket = Console.ReadLine();  // вид на билета "student" "standard" "kid" до команда "End"
+                int tickets = 0;  //   Създаме променливи за продените билети за конкретната прожекция
+
+                while (typeTicket != "End")
+                {
+                    if (typeTicket == "student")
+                    {
+                        student++;
+                    }
+                    else if (typeTicket == "standard")
+                    {
+                        standart++;
+                    }
+                    else
+                    {
+                        kid++;
+                    }
+                    tickets++;
+                    if (tickets == freeSeats)
+                    {
+                        break;
+                    }
+
+                    typeTicket = Console.ReadLine();
+                }
+                //  => когато излезем от вътрешния цикъл:
+                //     => Изчисляваме и отпечатваме какъв % от залата е запълнена (колко от общия брой билети са закупени)
+                double procent = tickets*100/freeSeats;
+                Console.WriteLine($"{input} - {procent:f2}% full.");
+
+                input = Console.ReadLine();
+            }
+
+            int totalTickets = student + standart + kid;
+            Console.WriteLine($"Total tickets: {totalTickets}");
+            Console.WriteLine($"{student / totalTickets * 100}% student tickets.");
+            Console.WriteLine($"{standart / totalTickets * 100}% student tickets.");
+            Console.WriteLine($"{kid / totalTickets * 100}% student tickets.");
+
+        }
+    }
+}
