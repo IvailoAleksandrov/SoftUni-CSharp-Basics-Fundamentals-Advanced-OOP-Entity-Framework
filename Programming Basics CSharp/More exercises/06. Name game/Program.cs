01f032cde90f04ad4a084417a929a23b9399f159
Ivailo Aleksandrov <ivailoaleksandrov123@gmail.com>
2022-08-10 09:59:53 +0300

    Exercises
    Add more

---
+++ Programming Basics CSharp/More exercises/06. Name game/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _06._Name_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int bestPoints = 0;
+            string winner = "";
+
+            while ("Stop" != name)
+            {
+                int currentPoints = 0;
+                for (int i = 0; i < name.Length; i++)
+                {
+                    int number = int.Parse(Console.ReadLine());
+                    if (number == name[i])
+                    {
+                        currentPoints += 10;
+                    }
+                    else
+                    {
+                        currentPoints += 2;
+                    }
+                }
+
+                if (currentPoints >= bestPoints)
+                {
+                    bestPoints = currentPoints;
+                    winner = name;
+                }
+                name = Console.ReadLine();
+            }
+            Console.WriteLine($"The winner is {winner} with {bestPoints} points!");
+
+
+
+
+        }
+    }
+}
---
+++ Programming Basics CSharp/More exercises/06. Traveling/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _06._Traveling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            
+
+            while (input != "End")
+            {
+                double budjet = double.Parse(Console.ReadLine());
+                double sum = 0;
+
+                while (sum < budjet )
+                {
+
+                    double savedMoney = double.Parse(Console.ReadLine());
+                    sum += savedMoney;
+                }
+                if (sum >= budjet)
+                {
+                    Console.WriteLine($"Going to {input}!");
+                }
+                input = Console.ReadLine();
+                
+
+            }
+
+
+
+        }
+    }
+}
