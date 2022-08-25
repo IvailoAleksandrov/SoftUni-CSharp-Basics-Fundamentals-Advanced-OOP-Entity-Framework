1dd7f83238228033b45903c6d4ea3561e5cbe53d
Ivailo Aleksandrov <ivailoaleksandrov123@gmail.com>
2022-08-25 19:36:45 +0300

    Add exercises

---
+++ Programming Fundamentals CSharp/Basic Syntax, Conditional Statements and Loops - Lab/09. Sum of Odd Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _09._Sum_of_Odd_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                int currentNum = (i * 2) - 1;
+               
+                Console.WriteLine(currentNum);
+                sum += currentNum;
+
+            }
+            Console.WriteLine($"Sum: {sum}");
+
+
+
+
+        }
+    }
+}
---
+++ Programming Fundamentals CSharp/Basic Syntax, Conditional Statements and Loops - Lab/11. Multiplication Table 2.0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _11._Multiplication_Table_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int startN = int.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine($"{n} X {startN} = {n * startN}");
+                startN++;
+            } while (startN <= 10);
+
+
+        }
+    }
+}
