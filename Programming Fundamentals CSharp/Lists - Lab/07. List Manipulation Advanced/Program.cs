a6382f2dccbd07fee068d14020da6facb2436d75
Ivailo Aleksandrov <ivailoaleksandrov123@gmail.com>
2022-10-11 19:13:40 +0300

    Exercises

--- Programming Fundamentals CSharp/Lists - Lab/07. List Manipulation Advanced/Program.cs
+++ Programming Fundamentals CSharp/Lists - Lab/07. List Manipulation Advanced/Program.cs
@@ -1,31 +1,169 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace _07._List_Manipulation_Advanced
+namespace _06._List_Manipulation_Basics
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var numbers = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            bool isListChanged = false;
 
-            // Remove odd numbers.
-            for (var index = numbers.Count - 1; index >= 0; index--)
+            while (true)
             {
-                if (numbers[index] % 2 == 1)
+                string command = Console.ReadLine();
+
+                if (command == "end")
                 {
-                    // Remove the element by specifying
-                    // the zero-based index in the list.
-                    numbers.RemoveAt(index);
+                    break;
+                }
+
+                string[] tokens = command.Split().ToArray();
+                string action = tokens[0];
+
+                switch (action)
+                {
+                    case "Add":
+                        int addNumber = int.Parse(tokens[1]);
+                        numbers.Add(addNumber);
+                        isListChanged = true;
+                        break;
+                    case "Remove":
+                        int remove = int.Parse(tokens[1]);
+                        numbers.Remove(remove);
+                        isListChanged = true;
+                        break;
+                    case "RemoveAt":
+                        int removeAtIndex = int.Parse(tokens[1]);
+                        numbers.RemoveAt(removeAtIndex);
+                        isListChanged = true;
+                        break;
+                    case "Insert":
+                        int index = int.Parse(tokens[2]);
+                        int numberToReplace = int.Parse(tokens[1]);
+                        numbers.Insert(index, numberToReplace);
+                        isListChanged = true;
+                        break;
+                    case "Contains":
+                        CheckContains(int.Parse(tokens[1]), numbers);
+                        break;
+                    case "PrintEven":
+                        PrintEvens(numbers);
+                        break;
+                    case "PrintOdd":
+                        PrintOdds(numbers);
+                        break;
+                    case "GetSum":
+                        int sum = GetSumOfNumbers(numbers);
+                        Console.WriteLine(sum);
+                        break;
+                    case "Filter":
+                        Filtered(tokens[1], int.Parse(tokens[2]), numbers);
+                        break;
+
                 }
             }
+            if (isListChanged)
+            {
+                Console.Write(string.Join(" ", numbers));
+            }
 
-            // Iterate through the list.
-            // A lambda expression is placed in the ForEach method
-            // of the List(T) object.
-            numbers.ForEach(
-                number => Console.Write(number + " "));
-            // Output: 0 2 4 6 8
+        }
+
+        private static void Filtered(string filter, int num, List<int> numbers)
+        {
+            switch (filter)
+            {
+                case "<":
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        if (numbers[i] < num)
+                        {
+                            Console.Write(numbers[i] + " ");
+                        }
+                    }
+                    Console.WriteLine();
+                    break;
+                case "<=":
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        if (numbers[i] <= num)
+                        {
+                            Console.Write(numbers[i] + " ");
+                        }
+                    }
+                    Console.WriteLine();
+                    break;
+                case ">":
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        if (numbers[i] > num)
+                        {
+                            Console.Write(numbers[i] + " ");
+                        }
+                    }
+                    Console.WriteLine();
+                    break;
+                case ">=":
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        if (numbers[i] > num)
+                        {
+                            Console.Write(numbers[i] + " ");
+                        }
+                    }
+                    Console.WriteLine();
+                    break;
+            }
+        }
+
+        private static int GetSumOfNumbers(List<int> numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+
+        private static void PrintOdds(List<int> numbers)
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] % 2 != 0)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+            }
+            Console.WriteLine();
+
+        }
+
+        private static void PrintEvens(List<int> numbers)
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        private static void CheckContains(int number, List<int> numbers)
+        {
+            if (numbers.Contains(number))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No such number");
+            }
         }
     }
 }
