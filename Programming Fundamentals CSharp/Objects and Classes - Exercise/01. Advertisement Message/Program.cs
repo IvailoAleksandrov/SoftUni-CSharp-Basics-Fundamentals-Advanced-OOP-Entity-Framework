f91d3a192fe21b33607c32b9da70185f7fd8cbf1
Ivailo Aleksandrov <ivailoaleksandrov123@gmail.com>
2022-11-08 16:13:40 +0200

    Exercises

---
+++ Programming Fundamentals CSharp/Objects and Classes - Exercise/01. Advertisement Message/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01._Advertisement_Message
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] phrases = { "Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.", "Exceptional product.", "I can't live without this product." };
+            string[] events = { "Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!" };
+            string[] authors = { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+            string[] cities = { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+
+            Random random = new Random();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string phrase = phrases[random.Next(0, phrases.Length)];
+                string eventt = events[random.Next(0, events.Length)];
+                string author = authors[random.Next(0, authors.Length)];
+                string citie = cities[random.Next(0, cities.Length)];
+
+
+                Console.WriteLine($"{phrase} {eventt} {author} {citie}");
+            }
+            
+        }
+    }
+}
---
+++ Programming Fundamentals CSharp/Objects and Classes - Exercise/02. Articles/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace _02._Articles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] article1 = Console.ReadLine().Split(", ");
+            int n = int.Parse(Console.ReadLine());
+
+            Article article = new Article(article1[0], article1[1], article1[2]);
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] commands = Console.ReadLine().Split(": ");
+                string action = commands[0];
+                string newText = commands[1];
+
+                switch (action)
+                {
+                    case "Edit":
+                        article.Edit(newText);
+                        break;
+                    case "ChangeAuthor":
+                        article.ChangeAuthor(newText);
+                        break;
+                    case "Rename":
+                        article.Rename(newText);
+                        break;
+                }
+            }
+            Console.WriteLine(article);
+
+        }
+
+    }
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public void Rename(string title)
+        {
+            Title = title;
+        }
+        public void Edit(string content)
+        {
+            Content = content;
+        }
+        public void ChangeAuthor(string author) => Author = author;
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+
+
+
+
+    }
+}
