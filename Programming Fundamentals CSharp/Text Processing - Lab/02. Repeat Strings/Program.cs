0d46a431fbf69ca6858d64acd990121cf97c106c
Ivailo Aleksandrov <ivailoaleksandrov123@gmail.com>
2022-11-16 09:27:11 +0200

    Exercises

---
+++ Programming Fundamentals CSharp/Text Processing - Lab/02. Repeat Strings/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _02._Repeat_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+
+            string repeated = string.Empty;
+
+            //foreach (var word in words)
+            //{
+            //    for (int i = 0; i < word.Length; i++)
+            //    {
+            //        repeated += word;
+            //    }
+            //}
+
+            //Console.WriteLine(repeated);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var word in words)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    sb.Append(word);
+                }
+            }
+            Console.WriteLine(sb);
+
+
+        }
+    }
+}
---
+++ Programming Fundamentals CSharp/Text Processing - Lab/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Digits__Letters_and_Other
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string digitsLetters = Console.ReadLine();
+
+            for (int i = 0; i < digitsLetters.Length; i++)
+            {
+                if (char.IsDigit(digitsLetters[i]))
+                {
+                    Console.Write(digitsLetters[i]);
+                }               
+            }
+            Console.WriteLine();
+            for (int i = 0; i < digitsLetters.Length; i++)
+            {
+                if (char.IsLetter(digitsLetters[i]))
+                {
+                    Console.Write(digitsLetters[i]);
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i < digitsLetters.Length; i++)
+            {
+                if (!char.IsDigit(digitsLetters[i]) && !char.IsLetter(digitsLetters[i]))
+                {
+                    Console.Write(digitsLetters[i]);
+                }
+            }
+        }
+    }
+}
