ad4a36186ebdf2c2fabcfe6dfc1fdbbe3653d35d
Ivailo Aleksandrov <ivailoaleksandrov123@gmail.com>
2023-01-07 15:53:12 +0200

    Exercises Generics

---
+++ C# Advanced/Generics - Exercise/04. Generic Swap Method Integer/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using P01_GenericBoxOfString;
+using System;
+using System.Linq;
+
+namespace _04._Generic_Swap_Method_Integer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+            Box<int> box = new Box<int>();
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                int numbers = int.Parse(Console.ReadLine());
+                box.Items.Add(numbers);
+            }
+            int[] indices = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Swap(box, indices[0], indices[1]);
+
+            Console.WriteLine(box.ToString());
+        }
+
+        public static void Swap(Box<int> box, int v1, int v2)
+        {
+            int temp = box.Items[v1];
+            box.Items[v1] = box.Items[v2];
+            box.Items[v2] = temp;
+        }
+    }
+}
---
+++ C# Advanced/Generics - Exercise/P05_GenericCountMethodString/Box.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P05_GenericCountMethodString
+{
+    public class Box<T> : IComparable<T>
+        where T : IComparable<T>
+    {
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; set; }
+        public int CompareTo(T other)
+        {
+            return Value.CompareTo(other);
+        }
+        public override string ToString()
+        {
+            return $"{Value.GetType().FullName}: {Value}";
+        }
+    }
+}
