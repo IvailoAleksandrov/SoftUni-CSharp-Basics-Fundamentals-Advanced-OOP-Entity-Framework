c761e375ef5095a1c706ee5b9e7ea794b476b15f
Ivailo Aleksandrov <ivailoaleksandrov123@gmail.com>
2023-01-25 15:46:07 +0200

    Exercises

---
+++ C# OOP/Polymorphism - Exercise/VehiclesExtension/Commands/VehicleCommands/DriveEmptyCommand.cs
@@ -0,0 +1,31 @@
+﻿namespace P02_VehiclesExtension.Commands.VehicleCommands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Models;
+
+    public class DriveEmptyCommand : Command
+    {
+        //--------------- Constructors -----------------
+        public DriveEmptyCommand() : base("DriveEmpty")
+        {
+            
+        }
+
+        //----------------- Methods --------------------
+        public override void Run(string[] args)
+        {
+            Console.WriteLine("For testing purposes");
+            //throw new NotImplementedException(); -> Liskov Substitution Violation if thrown...
+        }
+
+        public override void Run(string[] args, ICollection<Vehicle> vehicles)
+        {
+            foreach (Vehicle vehicle in vehicles.Where(v => v.GetType().Name == args[1]))
+            {
+                vehicle.DriveEmpty(double.Parse(args[2]));
+            }
+        }
+    }
+}
---
+++ C# OOP/Polymorphism - Exercise/VehiclesExtension/Commands/VehicleCommands/RefuelCommand.cs
@@ -0,0 +1,31 @@
+﻿namespace P02_VehiclesExtension.Commands.VehicleCommands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Models;
+
+    public class RefuelCommand : Command
+    {
+        //--------------- Constructors -----------------
+        public RefuelCommand() : base("Refuel")
+        {
+
+        }
+
+        //----------------- Methods --------------------
+        public override void Run(string[] args)
+        {
+            Console.WriteLine("For testing purposes");
+            //throw new NotImplementedException(); -> Liskov Substitution Violation if thrown...
+        }
+
+        public override void Run(string[] args, ICollection<Vehicle> vehicles)
+        {
+            foreach (Vehicle vehicle in vehicles.Where(v => v.GetType().Name == args[1]))
+            {
+                vehicle.Refuel(double.Parse(args[2]));
+            }
+        }
+    }
+}
