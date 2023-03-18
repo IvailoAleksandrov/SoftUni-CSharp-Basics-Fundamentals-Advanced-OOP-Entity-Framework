8e1416f1c1f5a09a2f82f7eff814e5aadf0147bb
Ivailo Aleksandrov <ivailoaleksandrov123@gmail.com>
2023-03-18 09:14:39 +0200

    Exercises

---
+++ Entity Framework Core/06.LINQ/MusicHub/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace MusicHub.Data;
+
+public static class Configuration
+{
+    public static string ConnectionString =
+        @"Server=DESKTOP-MDCQ18G\SQLEXPRESS;Database=MusicHub;Trusted_Connection=True";
+}
