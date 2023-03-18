a37c5a2add636dae9a6ce0f5f64993ffa0e4cb45
Ivailo Aleksandrov <ivailoaleksandrov123@gmail.com>
2023-03-18 07:41:11 +0200

    Exercises

---
+++ Entity Framework Core/05.Entity Relations - Lab and Exercise/Exercise/P02_FootballBetting/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using P02_FootballBetting.Data;
+using P02_FootballBetting.Data.Models;
+
+namespace P02_FootballBetting;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+       using FootballBettingContext footballBettingContext = new FootballBettingContext();
+
+        Team team = new Team();
+
+        team.Name= "Liverpool";
+        team.LogoUrl = "wwwww";
+        team.Initials = "LIVR";
+
+        footballBettingContext.Teams.Add(team);
+        footballBettingContext.SaveChanges();
+    }
+}
